b98bd35442588ad24a4be9ad247b1c14a409d4a6
Rodrigo Franco <caffeine@gmail.com>
2013-03-18 19:55:44 -0300

    Add Feed Model

---
+++ app/models/feed.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: feeds
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  url        :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Feed < ActiveRecord::Base
+  attr_accessible :name, :url
+end
