59817127fd7fbddcab0881039b49d713056b09e4
Rodrigo Franco <caffeine@gmail.com>
2013-03-23 19:44:38 -0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130318221047) do
+ActiveRecord::Schema.define(:version => 20130323213140) do
 
   create_table "articles", :force => true do |t|
     t.integer  "feed_id"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20130318221047) do
     t.text     "link"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.text     "notification"
   end
 
   create_table "feeds", :force => true do |t|
