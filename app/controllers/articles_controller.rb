86c6e00d348caf9960faabe134254d93b84afe28
Rodrigo Franco <caffeine@gmail.com>
2013-03-23 17:28:50 -0300

    Layout change

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,12 @@
 class ArticlesController < ApplicationController
-    
-    # Make sure you run this in the console while the app is available 
-    # thru your fwd.fw account. If you don't want to do it, generate 
-    # some articles and feeds or ask me for a dump --caffo
-    #
-    # Reader::Application::Configuration['feeds'].each do |f|
-    #   Reader::Application::Superfeedr.subscribe(f)
-    # end
+  
+  # Make sure you run this in the console while the app is available 
+  # thru your fwd.fw account. If you don't want to do it, generate 
+  # some articles and feeds or ask me for a dump --caffo
+  #
+  # Reader::Application::Configuration['feeds'].each do |f|
+  #   Reader::Application::Superfeedr.subscribe(f)
+  # end
 
   def index
     render :nothing => true
