68b90c80b954a1c525420e00b8c6a13e7ab1ed3b
Marcelo De Polli <marcelo.polli@gmail.com>
2013-03-27 22:44:49 -0300

    Add support to multiple entries per notification

--- app/models/article.rb
+++ app/models/article.rb
@@ -19,25 +19,24 @@ class Article < ActiveRecord::Base
   validates :title,           :presence => true
   validates :published_at,    :presence => true
   validates :content,         :presence => true
-  validates :link,            :presence => true
+  validates :link,            :presence => true,
+                              :uniqueness => true
 
   belongs_to :feed
 
   def self.create_from_raw_notification(raw_notification)
-    raw_notification.css('entry').each do |entry|
-      notification = Notification.new(entry)
+    raw_notification.css('entry').each_with_index do |entry, index|
+      notification = Notification.new(raw_notification, index)
+      feed = Feed.find_or_create_by_name_and_url(notification.feed_name, notification.feed_url)
       if Article.where(:link => notification.link).empty?
         article              = Article.new
-        article.feed         = Feed.find_or_create_by_name_and_url(notification.feed_name, notification.feed_url)
+        article.feed         = feed
         article.title        = notification.title
-        article.published_at = notification.pub_date
+        article.published_at = notification.published_at
         article.content      = notification.content
         article.link         = notification.link
         article.notification = raw_notification.document.to_s
         article.save
-        return article
-      else
-        return false
       end
     end
   end
