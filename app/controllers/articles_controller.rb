2d9b23f6a8d30467d5936e308b9a8a42519a74c7
Marcelo De Polli <marcelo.polli@gmail.com>
2013-03-30 19:52:30 -0300

    Change read attribute to work on false instead of nil

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 class ArticlesController < ApplicationController
   before_filter :authenticate, :except => [:subscribe]
-  
-  # Make sure you run this in the console while the app is available 
-  # thru your fwd.fw account. If you don't want to do it, generate 
+
+  # Make sure you run this in the console while the app is available
+  # thru your fwd.fw account. If you don't want to do it, generate
   # some articles and feeds or ask me for a dump --caffo
   #
   # Reader::Application::Configuration['feeds'].each do |f|
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
   # end
 
   def index
-    @articles = Article.where(:read => nil).order("published_at ASC")
+    @articles = Article.where(:read => false).order("published_at ASC")
   end
 
   def read
--- db/migrate/20130324183918_add_read_to_article.rb
+++ db/migrate/20130324183918_add_read_to_article.rb
@@ -1,5 +1,5 @@
 class AddReadToArticle < ActiveRecord::Migration
   def change
-    add_column :articles, :read, :boolean
+    add_column :articles, :read, :boolean, :default => false
   end
 end
