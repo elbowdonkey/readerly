de3454a0e0d29474f0321fc389fa049b24ef379b
Rodrigo Franco <caffeine@gmail.com>
2013-04-08 02:20:48 -0300

    Process feed list before showing the articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,6 @@
 class ArticlesController < ApplicationController
   before_filter :authenticate, :except => [:subscribe]
-
-  # Make sure you run this in the console while the app is available
-  # thru your fwd.fw account. If you don't want to do it, generate
-  # some articles and feeds or ask me for a dump --caffo
-  #
-  # APP_CONFIG['feeds'].each do |f|
-  #   Readerly::Application::Superfeedr.subscribe(f)
-  # end
+  before_filter :check_subscriptions, :only => [:index]
 
   def index
     @articles = Article.where(:read => false).order("published_at ASC")
@@ -18,5 +11,12 @@ class ArticlesController < ApplicationController
   end
 
   def subscribe
+  end
+
+  private
+  
+  def check_subscriptions
+    user = User.first_or_create
+    user.check_subscriptions! if user.dirty_feed_list?
   end
 end
--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,14 +1,20 @@
 require 'spec_helper'
 
 describe ArticlesController do
-  before do
+  before(:each) do
     http_login
+    Typhoeus::Request.stub(:post).and_return(OpenStruct.new(:body => "true"))
   end
 
   describe "GET index" do
     it "should be successful" do
       get :index
       response.should be_success
+    end
+
+    it "should populate the user feed list if none is found in it" do
+      get :index
+      User.first.feeds.should_not be_empty
     end
   end
 end
